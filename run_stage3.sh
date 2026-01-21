b4a32ae5a2a0a62157667d84f2b4b82fa397f82e
Xingyi Yao <156432170+Nominose@users.noreply.github.com>
2026-01-22 00:15:51 +0800

    update

--- run_stage3.sh
+++ run_stage3.sh
@@ -1,3 +1,3 @@
 ##### PLEASE UPDTAE CONFIGURE FILE BEFORE EXCUTING THIS SCRIPT #####
-python3 train_diff_model.py -p train -c config/mayo_ct_denoise_teacher
+python3 train_diff_model.py -p train -c config/mayo_ct_denoise.json
 ##### PLEASE UPDTAE CONFIGURE FILE BEFORE EXCUTING THIS SCRIPT #####
